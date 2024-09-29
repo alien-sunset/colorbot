164b24ae534db0794c2aa7c63ecad9a2726618bf
alien-sunset <alien_sunset@yahoo.com>
2024-09-29 13:49:11 -0400

    adjusted and added some more saturation and lightness options deleted:    logs/output1727564661660.txt modified:   paletts/draw-palette.png modified:   srcpg.js modified:   website/colorbot.js

--- website/colorbot.js
+++ website/colorbot.js
@@ -125,71 +125,113 @@ let sata = 0,
 
 let satSeed = rand(1, 22);
 
-let genrateSaturations = function () {
+const genrateSaturations = function () {
   if (satSeed == 1) {
     (sata = 95), (satb = 95), (satc = 95), (satd = 95), (sate = 95);
   }
-  if (satSeed == 2) {
+   if (satSeed == 2) {
+    (sata = 85), (satb = 85), (satc = 85), (satd = 85), (sate = 85);
+  } 
+  if (satSeed == 3) {
     (sata = 75), (satb = 75), (satc = 75), (satd = 75), (sate = 75);
   }
-  if (satSeed == 3) {
+  if (satSeed == 4) {
     (sata = 66), (satb = 66), (satc = 66), (satd = 66), (sate = 66);
   }
-  if (satSeed == 4) {
+  if (satSeed == 5) {
     (sata = 50), (satb = 50), (satc = 50), (satd = 50), (sate = 50);
   }
-  if (satSeed == 5) {
+  if (satSeed == 6) {
     (sata = 40), (satb = 40), (satc = 40), (satd = 40), (sate = 40);
   }
-  if (satSeed == 6) {
-    (sata = 25), (satb = 25), (satc = 25), (satd = 25), (sate = 25);
-  }
   if (satSeed == 7) {
-    (sata = 30), (satb = 40), (satc = 50), (satd = 60), (sate = 70);
+    (sata = 30), (satb = 30), (satc = 30), (satd = 30), (sate = 30);
   }
   if (satSeed == 8) {
-    (sata = 70), (satb = 60), (satc = 50), (satd = 40), (sate = 30);
+    (sata = 20), (satb = 20), (satc = 20), (satd = 20), (sate = 20);
   }
   if (satSeed == 9) {
-    (sata = 30), (satb = 50), (satc = 75), (satd = 50), (sate = 30);
+    (sata = 30), (satb = 40), (satc = 50), (satd = 60), (sate = 70);
   }
   if (satSeed == 10) {
-    (sata = 30), (satb = 75), (satc = 50), (satd = 75), (sate = 30);
+    (sata = 70), (satb = 60), (satc = 50), (satd = 40), (sate = 30);
   }
   if (satSeed == 11) {
-    (sata = 50), (satb = 75), (satc = 30), (satd = 75), (sate = 50);
+    (sata = 33), (satb = 50), (satc = 70), (satd = 50), (sate = 33);
   }
   if (satSeed == 12) {
-    (sata = 50), (satb = 30), (satc = 75), (satd = 30), (sate = 50);
+    (sata = 33), (satb = 70), (satc = 50), (satd = 70), (sate = 33);
   }
   if (satSeed == 13) {
-    (sata = 75), (satb = 50), (satc = 30), (satd = 50), (sate = 75);
+    (sata = 50), (satb = 70), (satc = 33), (satd = 70), (sate = 50);
   }
   if (satSeed == 14) {
-    (sata = 75), (satb = 30), (satc = 50), (satd = 30), (sate = 75);
+    (sata = 50), (satb = 33), (satc = 70), (satd = 33), (sate = 50);
   }
   if (satSeed == 15) {
-    (sata = 95), (satb = 85), (satc = 75), (satd = 66), (sate = 50);
+    (sata = 70), (satb = 50), (satc = 33), (satd = 50), (sate = 70);
   }
   if (satSeed == 16) {
-    (sata = 50), (satb = 66), (satc = 75), (satd = 80), (sate = 95);
+    (sata = 70), (satb = 33), (satc = 50), (satd = 33), (sate = 70);
   }
   if (satSeed == 17) {
-    (sata = 75), (satb = 85), (satc = 95), (satd = 85), (sate = 75);
+    (sata = 30), (satb = 40), (satc = 50), (satd = 40), (sate = 30);
   }
   if (satSeed == 18) {
-    (sata = 75), (satb = 95), (satc = 85), (satd = 95), (sate = 75);
+    (sata = 30), (satb = 50), (satc = 40), (satd = 50), (sate = 30);
   }
   if (satSeed == 19) {
-    (sata = 85), (satb = 95), (satc = 75), (satd = 95), (sate = 85);
+    (sata = 40), (satb = 30), (satc = 50), (satd = 30), (sate = 40);
   }
   if (satSeed == 20) {
-    (sata = 85), (satb = 75), (satc = 95), (satd = 75), (sate = 85);
+    (sata = 40), (satb = 50), (satc = 30), (satd = 50), (sate = 40);
   }
   if (satSeed == 21) {
-    (sata = 95), (satb = 85), (satc = 75), (satd = 85), (sate = 95);
+    (sata = 50), (satb = 40), (satc = 30), (satd = 40), (sate = 50);
   }
   if (satSeed == 22) {
+    (sata = 50), (satb = 30), (satc = 40), (satd = 30), (sate = 50);
+  }
+  if (satSeed == 23) {
+    (sata = 50), (satb = 60), (satc = 70), (satd = 60), (sate = 50);
+  }
+  if (satSeed == 24) {
+    (sata = 50), (satb = 70), (satc = 60), (satd = 70), (sate = 50);
+  }
+  if (satSeed == 25) {
+    (sata = 60), (satb = 50), (satc = 70), (satd = 50), (sate = 60);
+  }
+  if (satSeed == 26) {
+    (sata = 60), (satb = 70), (satc = 50), (satd = 70), (sate = 60);
+  }
+  if (satSeed == 27) {
+    (sata = 70), (satb = 60), (satc = 50), (satd = 60), (sate = 70);
+  }
+  if (satSeed == 28) {
+    (sata = 70), (satb = 50), (satc = 60), (satd = 50), (sate = 70);
+  }
+  if (satSeed == 29) {
+    (sata = 95), (satb = 85), (satc = 77), (satd = 66), (sate = 55);
+  }
+  if (satSeed == 30) {
+    (sata = 55), (satb = 66), (satc = 75), (satd = 85), (sate = 95);
+  }
+  if (satSeed == 31) {
+    (sata = 75), (satb = 85), (satc = 95), (satd = 85), (sate = 75);
+  }
+  if (satSeed == 32) {
+    (sata = 75), (satb = 95), (satc = 85), (satd = 95), (sate = 75);
+  }
+  if (satSeed == 33) {
+    (sata = 85), (satb = 95), (satc = 75), (satd = 95), (sate = 85);
+  }
+  if (satSeed == 34) {
+    (sata = 85), (satb = 75), (satc = 95), (satd = 75), (sate = 85);
+  }
+  if (satSeed == 35) {
+    (sata = 95), (satb = 85), (satc = 75), (satd = 85), (sate = 95);
+  }
+  if (satSeed == 36) {
     (sata = 95), (satb = 75), (satc = 85), (satd = 75), (sate = 95);
   }
 
@@ -204,7 +246,7 @@ let lita = 0,
 
 let litSeed = rand(1, 28);
 
-let genrateLightnesses = function () {
+const genrateLightnesses = function () {
   if (litSeed == 1) {
     (lita = 85), (litb = 85), (litc = 85), (litd = 85), (lite = 85);
   }
@@ -248,46 +290,82 @@ let genrateLightnesses = function () {
     (lita = 50), (litb = 80), (litc = 66), (litd = 80), (lite = 50);
   }
   if (litSeed == 15) {
-    (lita = 20), (litb = 30), (litc = 40), (litd = 50), (lite = 60);
+    (lita = 80), (litb = 70), (litc = 60), (litd = 70), (lite = 80);
   }
   if (litSeed == 16) {
-    (lita = 60), (litb = 50), (litc = 40), (litd = 30), (lite = 20);
+    (lita = 80), (litb = 60), (litc = 70), (litd = 60), (lite = 80);
   }
   if (litSeed == 17) {
-    (lita = 30), (litb = 40), (litc = 50), (litd = 40), (lite = 30);
+    (lita = 70), (litb = 60), (litc = 80), (litd = 60), (lite = 70);
   }
   if (litSeed == 18) {
-    (lita = 30), (litb = 50), (litc = 40), (litd = 50), (lite = 30);
+    (lita = 70), (litb = 80), (litc = 60), (litd = 80), (lite = 70);
   }
   if (litSeed == 19) {
-    (lita = 40), (litb = 50), (litc = 30), (litd = 50), (lite = 40);
+    (lita = 60), (litb = 70), (litc = 80), (litd = 70), (lite = 60);
   }
   if (litSeed == 20) {
-    (lita = 40), (litb = 30), (litc = 50), (litd = 30), (lite = 40);
+    (lita = 60), (litb = 80), (litc = 70), (litd = 80), (lite = 60);
   }
   if (litSeed == 21) {
-    (lita = 50), (litb = 40), (litc = 30), (litd = 40), (lite = 50);
+    (lita = 20), (litb = 30), (litc = 40), (litd = 50), (lite = 60);
   }
   if (litSeed == 22) {
-    (lita = 50), (litb = 30), (litc = 40), (litd = 30), (lite = 50);
+    (lita = 60), (litb = 50), (litc = 40), (litd = 30), (lite = 20);
   }
   if (litSeed == 23) {
-    (lita = 40), (litb = 50), (litc = 60), (litd = 50), (lite = 40);
+    (lita = 30), (litb = 40), (litc = 50), (litd = 40), (lite = 30);
   }
   if (litSeed == 24) {
-    (lita = 40), (litb = 60), (litc = 50), (litd = 60), (lite = 40);
+    (lita = 30), (litb = 50), (litc = 40), (litd = 50), (lite = 30);
   }
   if (litSeed == 25) {
-    (lita = 50), (litb = 40), (litc = 60), (litd = 40), (lite = 50);
+    (lita = 40), (litb = 50), (litc = 30), (litd = 50), (lite = 40);
   }
   if (litSeed == 26) {
-    (lita = 50), (litb = 60), (litc = 40), (litd = 60), (lite = 50);
+    (lita = 40), (litb = 30), (litc = 50), (litd = 30), (lite = 40);
   }
   if (litSeed == 27) {
-    (lita = 60), (litb = 50), (litc = 40), (litd = 50), (lite = 60);
+    (lita = 50), (litb = 40), (litc = 30), (litd = 40), (lite = 50);
   }
   if (litSeed == 28) {
+    (lita = 50), (litb = 30), (litc = 40), (litd = 30), (lite = 50);
+  }
+  if (litSeed == 29) {
+    (lita = 40), (litb = 50), (litc = 60), (litd = 50), (lite = 40);
+  }
+  if (litSeed == 30) {
+    (lita = 40), (litb = 60), (litc = 50), (litd = 60), (lite = 40);
+  }
+  if (litSeed == 31) {
+    (lita = 50), (litb = 40), (litc = 60), (litd = 40), (lite = 50);
+  }
+  if (litSeed == 32) {
+    (lita = 50), (litb = 60), (litc = 40), (litd = 60), (lite = 50);
+  }
+  if (litSeed == 33) {
+    (lita = 60), (litb = 50), (litc = 40), (litd = 50), (lite = 60);
+  }
+  if (litSeed == 34) {
     (lita = 60), (litb = 40), (litc = 50), (litd = 40), (lite = 60);
+  }
+  if (litSeed == 35) {
+    (lita = 70), (litb = 60), (litc = 50), (litd = 60), (lite = 70);
+  }
+  if (litSeed == 36) {
+    (lita = 70), (litb = 50), (litc = 60), (litd = 50), (lite = 70);
+  }
+  if (litSeed == 37) {
+    (lita = 60), (litb = 70), (litc = 50), (litd = 70), (lite = 60);
+  }
+  if (litSeed == 38) {
+    (lita = 60), (litb = 50), (litc = 70), (litd = 50), (lite = 60);
+  }
+  if (litSeed == 39) {
+    (lita = 50), (litb = 60), (litc = 70), (litd = 60), (lite = 50);
+  }
+  if (litSeed == 40) {
+    (lita = 50), (litb = 70), (litc = 60), (litd = 70), (lite = 50);
   }
 
   return lita, litb, litc, litd, lite;
